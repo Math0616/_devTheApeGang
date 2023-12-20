38533485aab3a385255732093bfbf94f0e2a7e71
Math0616 <151874665+Math0616@users.noreply.github.com>
2023-12-20 15:17:21 +0100

    update

--- history.js
+++ history.js
@@ -9,19 +9,47 @@ function displayHistoryData(historyData) {
     const gallery = document.querySelector('.gallery');
     gallery.innerHTML = ''; // Clear existing content
     gallery.style.display = 'grid';
-    gallery.style.gridTemplateColumns = '1fr'; // Set to single column
+    gallery.style.gridTemplateColumns = '1fr 2fr 1fr 1fr'; // Set grid columns
 
     historyData.forEach(item => {
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('gallery-item');
-        itemDiv.classList.add('show'); // Ensure it is visible
+        item.activities.forEach(activity => {
+            const row = document.createElement('div');
+            row.classList.add('gallery-item');
+            row.classList.add('show'); // Ensure it is visible
+            row.style.display = 'grid';
+            row.style.gridTemplateColumns = '1fr 2fr 1fr 1fr';
+            row.style.gap = '10px';
+            row.style.marginBottom = '10px';
 
-        // Add content to itemDiv based on item properties
-        // Example: itemDiv.textContent = item.someProperty;
+            // Image column
+            const imageContainer = document.createElement('div');
+            const img = document.createElement('img');
+            img.src = activity.token.contentURI;
+            img.alt = activity.token.meta.name;
+            imageContainer.appendChild(img);
+            row.appendChild(imageContainer);
 
-        gallery.appendChild(itemDiv);
+            // Name column
+            const nameDiv = document.createElement('div');
+            nameDiv.textContent = activity.token.meta.name;
+            row.appendChild(nameDiv);
+
+            // Listed Price column
+            const priceDiv = document.createElement('div');
+            priceDiv.textContent = activity.listedPrice ? `${activity.listedPrice} BTC` : 'N/A';
+            row.appendChild(priceDiv);
+
+            // Created At column
+            const dateDiv = document.createElement('div');
+            dateDiv.textContent = new Date(activity.createdAt).toLocaleString();
+            row.appendChild(dateDiv);
+
+            // Append row to the gallery
+            gallery.appendChild(row);
+        });
     });
 }
 
 // This function can be called when the history button is clicked
 document.getElementById('history-btn').addEventListener('click', loadHistoryData);
+
